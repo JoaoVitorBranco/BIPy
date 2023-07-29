a456b6ba2aa302e86dec0ed3e77e6d81f6198464
Vitor Soller <vgsoller@gmail.com>
2023-07-29 17:52:48 -0300

    impelment redo

--- src/GUI/Mem_Dados.py
+++ src/GUI/Mem_Dados.py
@@ -1,5 +1,5 @@
 from PyQt5 import QtCore, QtWidgets, QtGui
-from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtWidgets import QMessageBox, QUndoStack, QUndoCommand, QAction
 
 from src.GUI.MemInterface import Mem_Interface
 from src.GUI.box.WarningMessageBox import WarningMessageBox
@@ -24,6 +24,24 @@ class Mem_Dados(Mem_Interface):
             delegate = StyledItemDelegate(self.tableWidget)
             self.tableWidget.setItemDelegateForColumn(i, delegate)
 
+        # editMenu
+        self.editMenu = self.menuBar().addMenu("&Edit")
+        
+        # add ctrl z shortcut to undo last change using QUndoCommand
+        self.undoStack = QUndoStack(self)
+
+        self.undoAction = QAction("Desfazer", self)
+        self.undoAction.triggered.connect(self.undoStack.undo)
+        self.undoAction.setShortcut("Ctrl+Z")
+        self.editMenu.addAction(self.undoAction)
+
+        # # add ctrl y shortcut to redo last change using QUndoCommand # TODO
+        # self.redoAction = QAction("Refazer", self)
+        # self.redoAction.triggered.connect(self.undoStack.redo)
+        # self.redoAction.setShortcut("Ctrl+Y")
+        # self.editMenu.addAction(self.redoAction)
+
+
     # region Funções de controle
 
     def on_changed(self, item):
@@ -32,8 +50,26 @@ class Mem_Dados(Mem_Interface):
         valor = item.text()
         celula = f'0x{linha:02X}{coluna:X}'
 
-        self.altera_memoria_de_dados(celula, valor)
+        self.valor_anterior = self.memoria[celula[:-1]][celula[-1]]
 
+        if valor != self.valor_anterior:
+
+            command = QUndoCommand()
+
+            command.redo = lambda: self.altera_celular_e_item(celula, valor, item)
+            command.undo = lambda: self.altera_celular_e_item(celula, self.valor_anterior, item)
+
+            self.undoStack.push(command)
+
+
+    def altera_celular_e_item(self, endereco, valor, item):
+        self.tableWidget.clearSelection()
+        self.tableWidget.setCurrentItem(item)
+        print(self.undoStack.count())
+        self.altera_memoria_de_dados(endereco, valor)
+        item.setText(valor)
+
+        
     def zerar_memoria(self):
         msg = QMessageBox()
         msg.setWindowIcon(QtGui.QIcon(self.resource_path('src/GUI/assets/icone.ico')))
